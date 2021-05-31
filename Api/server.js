ba18d7b142718d9da4ab4cdb1eba579087e20ee8
Ana Beatriz <araujoanabeatriz48@gmail.com>
2021-05-30 21:03:15 -0300

    teste mudança do get module

--- Api/server.js
+++ Api/server.js
@@ -4,8 +4,8 @@ let port = process.env.PORT || 3000;
 
 const CoreApiChatbotRoute = require('./src/routes/CoreApiChatbotRoutes')
 
-app.use(CoreApiChatbotRoute);
-
+//app.use(CoreApiChatbotRoute);
+app.get('/avatar', (req,res) => {  res.send('Hello')});
 app.listen(port, () => {
   console.log(`Servidor rodando em  http:localhost:${port}`)
 })
