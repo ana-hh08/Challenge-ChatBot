6cc729c4d01f0e9d00f9a1875d5b22b6e841e2db
Ana Beatriz <araujoanabeatriz48@gmail.com>
2021-05-30 21:56:58 -0300

    teste deploy

--- Api/index.js
+++ Api/index.js
@@ -1,14 +1,15 @@
-const express = require('express')
+//const express = require('express')
+import express from 'express';
 let app = express();
 let port = process.env.PORT || 3000;
 
-const CoreApiChatbotRoute = require('./src/routes/CoreApiChatbotRoutes')
+//const CoreApiChatbotRoute = require('./src/routes/CoreApiChatbotRoutes')
 
-//app.use(CoreApiChatbotRoute);
+// app.use(CoreApiChatbotRoute);
 app.get('/avatar', (req,res) => {  res.send('Hello')});
 app.listen(port, () => {
   console.log(`Servidor rodando em  http:localhost:${port}`)
 })
 
-module.exports = app;
+export default app;
 
